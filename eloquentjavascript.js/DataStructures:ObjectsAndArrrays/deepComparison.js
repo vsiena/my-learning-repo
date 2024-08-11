dc6282bd78e51caec6a8225f32a9a17a61679c77
vsiena <vincent.siena@feather-insurance.com>
2024-08-11 14:57:45 +0200

    update elo js file

---
+++ eloquentjavascript.js/DataStructures:ObjectsAndArrrays/deepComparison.js
@@ -0,0 +1,25 @@
+/* Write a function deepEqual that takes two values 
+and returns true only if they are the same value or are objects with the same properties, 
+where the values of the properties are equal when compared with a recursive call to deepEqual. 
+
+To find out whether values should be compared directly (using the === operator for that) 
+or have their properties compared, you can use the typeof operator. 
+If it produces "object" for both values, you should do a deep comparison. 
+But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the properties of objects to compare them.*/
+
+
+let obj = {here: {is: "an"}, object: 2};
+
+function deepEqual(value1, value2){
+    return (value1 === value2)
+};          
+
+
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
