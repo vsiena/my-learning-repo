6ea928af1bc85d13be43f1ab64763e4fd515e830
vsiena <vincent.siena@feather-insurance.com>
2024-08-12 00:17:45 +0200

    update chessboard exercise

--- eloquentjavascript.js/Ch2:ProgramStructure/chessboard.js
+++ eloquentjavascript.js/Ch2:ProgramStructure/chessboard.js
@@ -21,4 +21,18 @@ change the program so that it works for any size,
 outputting a grid of the given width and height.
 */
 
+let size = 8;
+let board = "";
 
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
