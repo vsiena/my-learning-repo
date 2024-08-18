d9d4b3ab5b12538df3ed88943c3c13826ba329a5
vsiena <vincent.siena@feather-insurance.com>
2024-08-18 13:38:23 +0200

    Updated deepCompare

--- eloquentjavascript.js/Ch4:DataStructures:ObjectsAndArrays/deepComparison.js
+++ eloquentjavascript.js/Ch4:DataStructures:ObjectsAndArrays/deepComparison.js
@@ -7,15 +7,30 @@ or have their properties compared, you can use the typeof operator.
 If it produces "object" for both values, you should do a deep comparison. 
 But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
 
-The Object.keys function will be useful when you need to go over the properties of objects to compare them.*/
+The Object.keys function will be useful when you need to go over the properties of objects to compare them.
+
+Hint:
+
+Your test for whether you are dealing with a real object will 
+look something like typeof x == "object" && x != null. 
+Be careful to compare properties only when both arguments are objects. 
+In all other cases you can just immediately return the result of applying ===.
+
+Use Object.keys to go over the properties. 
+You need to test whether both objects have the same set of property names 
+and whether those properties have identical values. 
+One way to do that is to ensure that both objects have 
+the same number of properties (the lengths of the property lists are the same). 
+And then, when looping over one of the object’s properties to compare them, 
+always first make sure the other actually has a property by that name. 
+If they have the same number of properties and all properties in one also exist in the other, 
+they have the same set of property names.
+
+Returning the correct value from the function is best done 
+by immediately returning false when a mismatch is found and returning true at the end of the function.*/
 
 
 let obj = {here: {is: "an"}, object: 2};
-
-function deepEqual(value1, value2){
-    return (value1 === value2)
-};          
-
 
 console.log(deepEqual(obj, obj));
 // → true
