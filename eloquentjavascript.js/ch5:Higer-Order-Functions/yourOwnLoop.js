d57f59ba92a82f6930afa771ace397156033621e
vsiena <vincent.siena@feather-insurance.com>
2024-08-18 23:32:47 +0200

    Update ch5 exercises

--- eloquentjavascript.js/ch5:Higer-Order-Functions/yourOwnLoop.js
+++ eloquentjavascript.js/ch5:Higer-Order-Functions/yourOwnLoop.js
@@ -26,3 +26,11 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+function loopTwo(start, test, update, body){
+  for (let value = start; test(value); value = update(value)){
+    body(value);
+  }
+};
+
+loopTwo(3, n => n > 0, n => n - 1, console.log);
