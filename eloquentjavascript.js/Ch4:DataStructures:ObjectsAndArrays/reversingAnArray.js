99e0426ffafe26e1709ef41a1732bf1f0c65b9fe
vsiena <vincent.siena@feather-insurance.com>
2024-08-18 12:37:17 +0200

    Update Ch4 reversingAnArray exercise

--- eloquentjavascript.js/Ch4:DataStructures:ObjectsAndArrays/reversingAnArray.js
+++ eloquentjavascript.js/Ch4:DataStructures:ObjectsAndArrays/reversingAnArray.js
@@ -11,6 +11,15 @@ modify the array given as its argument by reversing its elements. Neither may us
 Thinking back to the notes about side effects and pure functions in the previous chapter, 
 which variant do you expect to be useful in more situations? Which one runs faster?
 
+Hint for reverseArrayInPlace: 
+The trick is to swap the first and last elements, then the second and second-to-last, and so on. 
+You can do this by looping over half the length of the array 
+(use Math.floor to round down—you don’t need to touch the middle element in an array with an odd number of elements) 
+and swapping the element at position i with the one at position array.length - 1 - i. 
+You can use a local binding to briefly hold onto one of the elements, 
+overwrite that one with its mirror image, and then put the value 
+from the local binding in the place where the mirror image used to be.
+
 */
 
 // Your code here.
@@ -23,7 +32,12 @@ function reverseArray(array){
 }
 
 function reverseArrayInPlace(array){
-    
+    for (let i = 0; i < Math.floor(array.length / 2); i++){
+        let tempBind = array[i];
+        array[i] = array[(array.length - 1) - i];
+        array[(array.length - 1) - i] = tempBind;
+    }
+    return array;
 }
 
 let myArray = ["A", "B", "C"];
