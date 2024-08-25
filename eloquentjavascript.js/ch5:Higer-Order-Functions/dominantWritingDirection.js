3ffa9f54d15f62afbd6f8e578870380574d8f2d1
vsiena <vincent.siena@feather-insurance.com>
2024-08-25 22:59:07 +0200

    added solution

--- eloquentjavascript.js/ch5:Higer-Order-Functions/dominantWritingDirection.js
+++ eloquentjavascript.js/ch5:Higer-Order-Functions/dominantWritingDirection.js
@@ -18,7 +18,14 @@ where reduce was used to find the script with the most characters.
 const { SCRIPTS } = require('./scripts.js')
 
 function dominantDirection(text) {
-  
+  let counted = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({name}) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => a.count > b.count ? a : b).name;
 }
 
 
